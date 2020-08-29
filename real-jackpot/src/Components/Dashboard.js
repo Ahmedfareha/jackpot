b58a7e41a89cf5c67138bbe7088054c81c9daa99
Ahmedfareha <farehaahmed47.fa@gmail.com>
2020-08-29 09:45:19 +0530

    Dashboard rendering problem fixed
    Signed-off-by: Ahmedfareha <farehaahmed47.fa@gmail.com>

--- real-jackpot/src/Components/Dashboard.js
+++ real-jackpot/src/Components/Dashboard.js
@@ -7,46 +7,45 @@ const Dashboard = (props) => {
             date =  today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate()+' '+today.getHours()+":"+today.getMinutes()+":"+today.getSeconds()+' '+(today.getHours()>=12?'PM':'AM');
     var d=props.dashboardData
     // console.log(d[0].length)
+    if(d[0]!=undefined){
+        if(d[0][0]!=undefined){
+            var dashboardCards = new Array(d.length)
+            for(var i=0; i<d[0].length; i++){
+                if(d[0][i].status=='update'||d[0][i].status=='wait'){
+                    dashboardCards[i] = new Array(3)
+                if(d[0][i].region!=undefined)
+                    dashboardCards[i][0] = d[0][i].region
+                if(d[0][i].status!=undefined)
+                    dashboardCards[i][1] = d[0][i].status
+                if(d[0][i].luckyNo!=undefined)
+                    dashboardCards[i][2] = d[0][i].luckyNo 
+                }
     
-    if(d[0][0]!=undefined){
-        var dashboardCards = new Array(d.length)
-        for(var i=0; i<d[0].length; i++){
-            if(d[0][i].status=='update'||d[0][i].status=='wait'){
-                dashboardCards[i] = new Array(3)
-            if(d[0][i].region!=undefined)
-                dashboardCards[i][0] = d[0][i].region
-            if(d[0][i].status!=undefined)
-                dashboardCards[i][1] = d[0][i].status
-            if(d[0][i].luckyNo!=undefined)
-                dashboardCards[i][2] = d[0][i].luckyNo 
+                // console.log(dashboardCards[i])
             }
-
-            // console.log(dashboardCards[i])
-        }
-        // console.log(dashboardCards)
-        const trails = dashboardCards.map(t=>{
+            // console.log(dashboardCards)
+            const trails = dashboardCards.map(t=>{
+                return(
+                    <DashCard region={t[0]} number={t[2]} status={t[1]}></DashCard>
+                )}
+            )
+            // console.log(trails)
             return(
-                <DashCard region={t[0]} number={t[2]} status={t[1]}></DashCard>
-            )}
-        )
-        // console.log(trails)
-        return(
-            <div className="row dashboard">
-                <Title title="LIVE UPDATES" subtitle={date}></Title>
                 <div className="row dashboard">
-                    {trails}
+                    <Title title="LIVE UPDATES" subtitle={date}></Title>
+                    <div className="row dashboard">
+                        {trails}
+                    </div>
                 </div>
-            </div>
-        )
+            )
+        }
     }
-//    if(d[0][0]!=undefined){
-//        console.log(d[0][0].luckyNo)
-//    }
-   else return(
+
+    return(
         <div className="row dashboard">
             <Title title="LIVE UPDATES" subtitle={date}></Title>
             <div className="row dashboard">
-                 LOADING......
+                    <h1><b  style={{color:"black"}}>LOADING......</b></h1>
             {/* <Card region="Delhi" status="updated" number="007"></Card>
             <Card region="Mumbai" status="not updated"></Card>
             <Card region="Ghaziabad" status="updated" number="723"></Card>
